8bae2554c3a8265aed379b6fbbd0790cbb34f47e
PauloVict0r <paulovictor159@hotmail.com>
2026-04-26 13:25:50 -0300

    Integração de led e loop de testes com github actions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Teste")
+from machine import Pin
+import time
+
+def test_led():
+    # Inicializa o LED no GPIO 22 como saida
+    led_verde = Pin(22, Pin.OUT)
+    
+    # Pisca o LED
+    led_verde.value(1)
+    time.sleep(0.5)
+    led_verde.value(0)
+    time.sleep(0.5)
+    led_verde.value(1)
+    
+    print("LED_TEST_OK")
+
+if __name__ == '__main__':
+    test_led()
