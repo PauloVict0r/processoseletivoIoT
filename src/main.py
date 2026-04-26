84664f9486bb04e853fab773d165f5c618b2e331
PauloVict0r <paulovictor159@hotmail.com>
2026-04-26 14:08:31 -0300

    Integração do buzzer e atualização do teste para o actions

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
-from machine import Pin
+from machine import Pin, PWM
 import time
 
-def test_led():
-    # Inicializa o LED no GPIO 22 como saida
-    led_verde = Pin(22, Pin.OUT)
+def test_buzzer():
+    # Inicializa o Buzzer no GPIO 23 com PWM
+    buzzer = PWM(Pin(23))
     
-    # Pisca o LED
-    led_verde.value(1)
-    time.sleep(0.5)
-    led_verde.value(0)
-    time.sleep(0.5)
-    led_verde.value(1)
+    # Emite um bipe de 1kHz com 50% de duty cycle
+    buzzer.freq(1000)
+    buzzer.duty_u16(32768)
+    time.sleep(1)
     
-    print("LED_TEST_OK")
+    # Desliga o bipe
+    buzzer.duty_u16(0)
+    time.sleep(0.5)
+    
+    print("BUZZER_TEST_OK")
 
 if __name__ == '__main__':
-    test_led()
+    test_buzzer()
